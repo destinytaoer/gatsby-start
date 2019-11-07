aa557fd9330547e2cd4981951f52dda3e0b864a7
destinytaoer <1848765519@qq.com>
2019-11-07 21:39:11 +0800

    [plugins] add typography to beautify pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModile: `src/utils/typography`,
+      },
+    },
+  ],
 }
