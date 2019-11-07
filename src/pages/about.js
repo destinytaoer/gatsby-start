842071d3b38c4777a6fc55ff20cd38a699e3e80e
destinytaoer <1848765519@qq.com>
2019-11-07 20:08:45 +0800

    [pages] use Header in pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import Header from '../components/header'
 export default () => {
   return (
     <div>
+      <Header />
       <h1>About Destiny</h1>
       <p>location: SHEN ZHEN</p>
       <p>Graduation: ShenZhen University</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from 'react'
+import Header from '../components/header'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Header />
+    Hello world!
+  </div>
+)
