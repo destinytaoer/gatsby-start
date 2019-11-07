74f962b697fc5d873592f2e49882b177f0319e48
destinytaoer <1848765519@qq.com>
2019-11-07 22:03:19 +0800

    [components] update Header's structure and style

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-export default props => {
-  return (
-    <div>
-      <Link to="/">
-        <h1>{props.headerText}</h1>
-      </Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
-  )
-}
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+export default props => (
+  <header style={{ marginBottom: `1.5rem` }}>
+    <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+      <h1 style={{ display: `inline` }}>{props.headerText}</h1>
+    </Link>
+    <ul style={{ listStyle: `none`, float: `right` }}>
+      <ListLink to="/about">About</ListLink>
+      <ListLink to="/contact">Contact</ListLink>
+    </ul>
+  </header>
+)
