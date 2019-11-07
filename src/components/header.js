5fc88e48a13fa3abb3e0cd01af4e6477bd095428
destinytaoer <1848765519@qq.com>
2019-11-07 20:23:51 +0800

    [pages] create contact page, and link it in Header

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,7 @@ export default props => {
         <h1>{props.headerText}</h1>
       </Link>
       <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </div>
   )
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/header'
+
+export default () => {
+  return (
+    <div>
+      <Header headerText="Destiny'Note" />
+      <h1>Contact Me</h1>
+      <p>send me a message!</p>
+      <ul>
+        <li>QQ: 1848765519</li>
+        <li>WeChat: szu-zwl</li>
+        <li>Email: 1848765519@qq.com</li>
+      </ul>
+    </div>
+  )
+}
