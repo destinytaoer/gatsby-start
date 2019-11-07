ae1665f8b6b709b7f5fe821923bfc14718aedee4
destinytaoer <1848765519@qq.com>
2019-11-07 20:05:47 +0800

    create about

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default () => {
+  return (
+    <div>
+      <h1>About Destiny</h1>
+      <p>location: SHEN ZHEN</p>
+      <p>Graduation: ShenZhen University</p>
+    </div>
+  )
+}
